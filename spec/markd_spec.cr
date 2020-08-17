57230b8b6c8895318976d2ef5a0de9359ba5a7ec
Ryan Westlund <rlwestlund@gmail.com>
2020-08-17 14:29:00 -0400

    remove unwated tests

--- spec/markd_spec.cr
+++ spec/markd_spec.cr
@@ -3,8 +3,5 @@ require "./spec_helper"
 # Commonmark spec exapmles
 describe_spec("fixtures/spec.txt")
 
-# Smart punctuation exapmles
-describe_spec("fixtures/smart_punct.txt", smart: true)
-
 # Regression exapmles
 describe_spec("fixtures/regression.txt")
