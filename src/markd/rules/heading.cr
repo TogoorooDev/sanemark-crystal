489f338c3fca6dec67d6476cece555ccf74b8b49
Ryan Westlund <rlwestlund@gmail.com>
2020-08-19 15:14:47 -0400

    remove setext headings

--- src/markd/rules/heading.cr
+++ src/markd/rules/heading.cr
@@ -3,7 +3,6 @@ module Markd::Rule
     include Rule
 
     ATX_HEADING_MARKER    = /^\#{1,6}(?:[ \t]+|$)/
-    SETEXT_HEADING_MARKER = /^(?:=+|-+)[ \t]*$/
 
     def match(parser : Parser, container : Node) : MatchValue
       if match = match?(parser, ATX_HEADING_MARKER)
@@ -19,23 +18,6 @@ module Markd::Rule
           .sub(/ +#+ *$/, "")
 
         parser.advance_offset(parser.line.size - parser.offset)
-
-        MatchValue::Leaf
-      elsif (match = match?(parser, SETEXT_HEADING_MARKER)) &&
-            container.type.paragraph? && (parent = container.parent?) &&
-            !parent.type.block_quote?
-        # Setext Heading matched
-        parser.close_unmatched_blocks
-        heading = Node.new(Node::Type::Heading)
-        heading.source_pos = container.source_pos
-        heading.data["level"] = match[0][0] == '=' ? 1 : 2
-        heading.text = container.text
-
-        container.insert_after(heading)
-        container.unlink
-
-        parser.tip = heading
-        parser.advance_offset(parser.line.size - parser.offset, false)
 
         MatchValue::Leaf
       else
