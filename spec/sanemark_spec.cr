1cc5fdbcbcf13ab5a7ac76e5eb7533f1c1898759
Ryan Westlund <rlwestlund@gmail.com>
2020-10-03 10:33:58 -0400

    Remove reference to reference_links.md
    Not sure why this wasn't causing failures before

--- spec/sanemark_spec.cr
+++ spec/sanemark_spec.cr
@@ -3,8 +3,5 @@ require "./spec_helper"
 # Sanemark spec exapmles
 describe_spec("fixtures/spec.md")
 
-# Markd extension exapmles
-describe_spec("fixtures/reference_links.md")
-
 # Regression exapmles
 describe_spec("fixtures/regression.md")
